a4bb75afb6727673789254a4055ab54d7f5b329b
Scott Vaverchak <Scott.vaverchak@gmail.com>
2023-08-28 23:28:09 -0400

    moremalmaps

--- src/texture.rs
+++ src/texture.rs
@@ -57,11 +57,12 @@ impl Texture {
         device: &wgpu::Device, 
         queue: &wgpu::Queue, 
         bytes: &[u8], 
-        label: &str
+        label: &str,
+        is_normal_map: bool,
     ) -> Result<Self> { 
 
         let img = image::load_from_memory(bytes)?;
-        Self::from_image(device, queue, &img, Some(label))
+        Self::from_image(device, queue, &img, Some(label), is_normal_map)
 
     }
 
@@ -69,11 +70,18 @@ impl Texture {
         device: &wgpu::Device, 
         queue: &wgpu::Queue, 
         img: &image::DynamicImage, 
-        label: Option<&str>
+        label: Option<&str>,
+        is_normal_map: bool,
     ) -> Result<Self> {
-
+        
         let rgba = img.to_rgba8();
         let dimensions = img.dimensions();
+
+        let format = if is_normal_map { 
+            wgpu::TextureFormat::Rgba8Unorm    
+        } else { 
+            wgpu::TextureFormat::Rgba8UnormSrgb
+        };
 
         let size = wgpu::Extent3d { 
             width: dimensions.0, 
@@ -88,7 +96,7 @@ impl Texture {
                 mip_level_count: 1, 
                 sample_count: 1, 
                 dimension: wgpu::TextureDimension::D2, 
-                format: wgpu::TextureFormat::Rgba8UnormSrgb,
+                format,
                 usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
                 view_formats: &[],
             }
